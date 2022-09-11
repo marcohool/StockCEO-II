c235bb7e26bdb60a4ee52501eb7fcf5bc46e9c53
Marco <marco.boutchelski@gmail.com>
2022-09-12 00:07:10 +0100

    Stats command draws standard graph and sends it to guild

--- cogs/Stocks.py
+++ cogs/Stocks.py
@@ -2,6 +2,9 @@ from discord.ext import commands
 import yfinance as yf
 import yfinance.shared as shared
 import plotly.graph_objs as go
+import mplfinance as fplt
+import os
+import discord
 
 class Stocks(commands.Cog):
    def __init__(self, bot):
@@ -9,7 +12,7 @@ class Stocks(commands.Cog):
 
    @commands.command()
    async def stats(self, ctx, ticker: str):
-      data = yf.download(tickers = ticker, period='30d', interval='60m')
+      data = yf.download(tickers = ticker, period='30d', interval='1h')
 
       # If ticker not found
       if list(shared._ERRORS.keys()):
@@ -22,18 +25,26 @@ class Stocks(commands.Cog):
       # Get company name
       company = ticker.info['shortName']
 
-      # Generate graph
-      fig = go.Figure()
+      # Set plot 
+      fplt.plot(
+            data,
+            type='candle',
+            style='charles',
+            mav = 4,
+            figsize = (12, 9),
+            volume = True,
+            ylabel_lower='Volume',
+            scale_padding={'left': -0.25, 'top': 0, 'right': 0.75, 'bottom': 0.05},
+            ylabel= f"Share Price ({ticker.info['currency']})",
+            savefig='./images/graph.png'
+        )
 
-      # Candle sticks
-      fig.add_trace(go.Candlestick(x = data.index, open = data['Open'], high = data['High'], low = data['Low'], close = data['Close'], name = "Market Data"))
-
-      # Add titles
-      fig.update_layout(dict(xaxis=dict(type="category")), title = company, yaxis_title = f"Stock Price ({ticker.info['currency']})")
-      fig.update_xaxes(visible=False)
-
-      # Show graph
-      fig.show()
+      # Send image to Discord
+      embed = discord.Embed(
+            title=(f"{company} | {ticker.info['currency']}"), color=discord.Color(value=int("7EE622", 16)))
+      file = discord.File("images/graph.png", filename="graph.png")
+      embed.set_image(url="attachment://graph.png")
+      await ctx.send(file=file, embed=embed)
 
 
 async def setup(bot):
