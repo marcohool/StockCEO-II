c13a58d3833ee1071f8f3fb46a0ed7e5beff7baf
Marco <marco.boutchelski@gmail.com>
2022-09-12 13:43:36 +0100

    Restructed graph creation into own function

--- cogs/Stocks.py
+++ cogs/Stocks.py
@@ -13,24 +13,16 @@ class Stocks(commands.Cog):
    @commands.command()
    async def stats(self, ctx, ticker: str):
       
-      # Set period and interval for graph generation
-      period = "1y"
-      interval = "1d"
-
-      # Download data for given ticker
-      data = yf.download(tickers = ticker, period = period, interval = interval)
-
-      # If ticker not found
-      if list(shared._ERRORS.keys()):
-         await ctx.send("Stock ticker not found")
+      # Generate past 1 year graph image for ticker
+      try:
+         ticker = generateGraph(ticker, "1y")
+      except TickerException:
+         await ctx.send("Invalid ticker")
          return
-
-      # Get ticker      
-      ticker = yf.Ticker(ticker)
-
+      
       # Get long description for ticker
       desc = ticker.info.get('longBusinessSummary')
-      
+   
       # Shorten description to one sentence
       if desc:
          desc = desc.split('.').pop(0)
@@ -41,21 +33,10 @@ class Stocks(commands.Cog):
       else:
          desc = ticker.info['description']
 
-      # Set plot 
-      fplt.plot(
-            data,
-            type='candle',
-            style='yahoo',
-            mav = 4,
-            figsize = (12, 9),
-            volume = True,
-            title= f"{ticker.info.get('shortName')} | {data.index[0].date()} to {data.index[-1].date()}",
-            ylabel_lower='Volume',
-            xrotation = 0,
-            scale_padding={'left': 0, 'top': 0, 'right': 0.75, 'bottom': 0.2},
-            ylabel= f"Share Price ({ticker.info['currency']})",
-            savefig='./images/graph.png'
-        )
+      # Get formatted market cap
+      marketCap = ticker.info.get('marketCap')
+      if marketCap:
+         marketCap = format(ticker.info.get('marketCap'))
 
       # Build Discord embedded message
       embed = discord.Embed(title=(f"{ticker.info['shortName']} | {ticker.info['currency']}"),
@@ -65,20 +46,56 @@ class Stocks(commands.Cog):
       embed.add_field(name="Close", value=ticker.info.get('previousClose'), inline=True)
       embed.add_field(name="Day Low", value=ticker.info.get('dayLow'), inline=True)
       embed.add_field(name="Day High", value=ticker.info.get('dayHigh'), inline=True)
-      embed.add_field(name="Market Cap", value=format(ticker.info.get('marketCap')), inline=True)
+      embed.add_field(name="Market Cap", value=marketCap, inline=True)
       embed.add_field(name="52-Wk High", value=ticker.info.get('fiftyTwoWeekHigh'), inline=True)
       embed.add_field(name="52-Wk Low", value=ticker.info.get('fiftyTwoWeekLow'), inline=True)
       embed.add_field(name="Pre-market Price", value=ticker.info.get('preMarketPrice'), inline=True)
-
-
-
-      # Send image to Discord
-      # embed = discord.Embed(
-      #       title=(f"{ticker.info['shortName']} | {ticker.info['currency']}"), color=discord.Color(value=int("7EE622", 16)))
-      file = discord.File("images/graph.png", filename="graph.png")
       embed.set_image(url="attachment://graph.png")
+
+      # Send embed with file
+      file = discord.File("images/graph.png", filename="graph.png")
       await ctx.send(file=file, embed=embed)
 
+
+
+   @commands.command()
+   async def graph(self, ctx, ticker: str, duration: str):
+      pass
+
+
+def generateGraph(ticker, peroid):
+   
+   # Set period and interval for graph generation
+   period = "1y"
+   interval = "1d"
+
+   # Download data for given ticker
+   data = yf.download(tickers = ticker, period = period, interval = interval)
+
+   # If ticker not found
+   if list(shared._ERRORS.keys()):
+      raise TickerException
+
+   # Get ticker      
+   ticker = yf.Ticker(ticker)
+
+   # Set plot 
+   fplt.plot(
+         data,
+         type='candle',
+         style='yahoo',
+         mav = 4,
+         figsize = (12, 9),
+         volume = True,
+         title= f"{ticker.info.get('shortName')} | {data.index[0].date()} to {data.index[-1].date()}",
+         ylabel_lower='Volume',
+         xrotation = 0,
+         scale_padding={'left': 0, 'top': 0, 'right': 0.75, 'bottom': 0.2},
+         ylabel= f"Share Price ({ticker.info['currency']})",
+         savefig='./images/graph.png'
+      )
+
+   return ticker
 
 # Format large number into more readable value
 def format(n):
@@ -90,6 +107,8 @@ def format(n):
 
    return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
 
-
 async def setup(bot):
    await bot.add_cog(Stocks(bot))
+
+class TickerException(Exception):
+   pass
